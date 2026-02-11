13a69709a3a9b0d03125f74fd6afda90ccab5038
Darryl Blonski <darryl.blonski@parsons.us>
2026-02-11 14:04:42 -0500

    Moves settings to the settings file and out of build.sh and the Dockerfile.

--- build.sh
+++ build.sh
@@ -1,36 +1,47 @@
 #!/bin/bash
 
 declare date="$( date +%Y%m%d )"
-POSTGRES_VERSION="${POSTGRES_VERSION:-18}"
+
+. settings.sh
+
+export CLANG_VERSION="${CLANG_VERSION:-19}"
+export DOCKER_REPO="${DOCKER_REPO:-dblonski}"
+export GOLANG_VERSION="${GOLANG_VERSION:-1.24}"
+export GOSU_VERSION="${GOSU_VERSION:-1.17}"
+export POSTGRES_VERSION="${POSTGRES_VERSION:-18}"
 
 function build {
   docker build \
     --no-cache \
+    --build-arg CLANG_VERSION=${CLANG_VERSION} \
+    --build-arg DOCKER_REPO=${DOCKER_REPO} \
+    --build-arg GOLANG_VERSION=${GOLANG_VERSION} \
+    --build-arg GOSU_VERSION=${GOSU_VERSION} \
     --build-arg POSTGRES_VERSION=${POSTGRES_VERSION} \
-    -t dblonski/postgresql-plugins:pg${POSTGRES_VERSION}-${date} \
+    -t ${DOCKER_REPO}/postgresql-plugins:pg${POSTGRES_VERSION}-${date} \
     .
 }
 
 function tag_only {
-  local POSTGRES_FULL_VERSION="$( docker run -it dblonski/postgresql-plugins:pg${POSTGRES_VERSION}-${date} psql --version | awk '{ print $3; }' | tr '\n\r' '  ' )"
+  local POSTGRES_FULL_VERSION="$( docker run -it ${DOCKER_REPO}/postgresql-plugins:pg${POSTGRES_VERSION}-${date} psql --version | awk '{ print $3; }' | tr '\n\r' '  ' )"
   local TAGS=(
     "pg${POSTGRES_VERSION}-latest"
     "pg${POSTGRES_FULL_VERSION}"
     )
   for tag in ${TAGS[@]}; do
-    docker tag dblonski/postgresql-plugins:pg${POSTGRES_VERSION}-${date} dblonski/postgresql-plugins:${tag}
+    docker tag ${DOCKER_REPO}/postgresql-plugins:pg${POSTGRES_VERSION}-${date} ${DOCKER_REPO}/postgresql-plugins:${tag}
   done
 }
 
 function publish {
-  local POSTGRES_FULL_VERSION="$( docker run -it dblonski/postgresql-plugins:pg${POSTGRES_VERSION}-${date} psql --version | awk '{ print $3; }' | tr '\n\r' '  ' )"
+  local POSTGRES_FULL_VERSION="$( docker run -it ${DOCKER_REPO}/postgresql-plugins:pg${POSTGRES_VERSION}-${date} psql --version | awk '{ print $3; }' | tr '\n\r' '  ' )"
   local TAGS=(
     "pg${POSTGRES_VERSION}-latest"
     "pg${POSTGRES_FULL_VERSION}"
     )
   for tag in ${TAGS[@]}; do
-    docker tag dblonski/postgresql-plugins:pg${POSTGRES_VERSION}-${date} dblonski/postgresql-plugins:${tag}
-    docker image push dblonski/postgresql-plugins:${tag}
+    docker tag ${DOCKER_REPO}/postgresql-plugins:pg${POSTGRES_VERSION}-${date} ${DOCKER_REPO}/postgresql-plugins:${tag}
+    docker image push ${DOCKER_REPO}/postgresql-plugins:${tag}
   done
 }
 
